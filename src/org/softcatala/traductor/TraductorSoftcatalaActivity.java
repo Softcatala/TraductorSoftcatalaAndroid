c617ea2634a6b5e0da33870fe8c997dc80c68aa1
Jordi Mas <jmas@softcatala.org>
2011-08-27 19:27:20 +0200

    Support for Google Voice

--- src/org/softcatala/traductor/TraductorSoftcatalaActivity.java
+++ src/org/softcatala/traductor/TraductorSoftcatalaActivity.java
@@ -18,14 +18,22 @@
  */
 package org.softcatala.traductor;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.Spinner;
 import org.softcatala.utils.AndroidUtils;
 
@@ -33,19 +41,30 @@ public class TraductorSoftcatalaActivity extends Activity {
 
     EditText translatedTextEdit;
     EditText textToTranslateEdit;
+    ImageButton speakButton;
     Spinner languagesSpinner;
     String _langCode;
     AlertDialog ad;
+    VoiceRecognition voiceRecognition;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        
+        voiceRecognition = new VoiceRecognition (this);
         setContentView(R.layout.main);
         translatedTextEdit = (EditText) findViewById(R.id.translatedTextEdit);
         textToTranslateEdit = (EditText) findViewById(R.id.textToTranslateEdit);
-        InitSpinner();
+        speakButton = (ImageButton) findViewById(R.id.voiceButton);
 
+        InitSpinner();
+        
+        PackageManager pm = getPackageManager();
+        List<ResolveInfo> activities = pm.queryIntentActivities(
+                new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH), 0);        
+        
+        speakButton.setEnabled(activities.size() != 0);        
     }
 
     public void setLangCode(String langCode) {
@@ -100,4 +119,40 @@ public class TraductorSoftcatalaActivity extends Activity {
             ad.show();
         }
     }
+    
+    private static final int VOICE_RECOGNITION_REQUEST_CODE = 1714;
+    
+    public void OnVoiceRecognition (View v) {
+    	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,  RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        
+        String lang = voiceRecognition.GetSupportedLangFromSCTranslator (GetSourceSelectedLang ());
+        
+        if (lang.isEmpty() == false) {
+        	intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, lang);
+        }
+        
+        startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);    
+    }
+    
+    private String GetSourceSelectedLang ()
+    {
+    	int pos = _langCode.indexOf("|");
+    	return _langCode.substring(0, pos); 	
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {    	
+    	super.onActivityResult(requestCode, resultCode, data);
+    	
+        if ((requestCode == VOICE_RECOGNITION_REQUEST_CODE && resultCode == RESULT_OK) == false)
+        	return;
+
+        ArrayList<String> matches = data.getStringArrayListExtra(
+                RecognizerIntent.EXTRA_RESULTS);
+        
+        if (matches.isEmpty() == false)
+        	textToTranslateEdit.setText(matches.get(0));        
+    }
+    
 }
