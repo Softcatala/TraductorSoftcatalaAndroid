63f081a5775196f34b86712b0d830cc64a0231e3
Jordi Mas <jmas@softcatala.org>
2016-11-01 12:14:28 +0100

    Editable text with clear

---
+++ app/src/main/java/org/softcatala/traductor/ClearableEditText.java
@@ -0,0 +1,196 @@
+/*
+ * Copyright (C) 2016 Jordi Mas i Hernàndez <jmas@softcatala.org>
+ *           (C) Alex Yanchenko
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+
+package org.softcatala.traductor;
+
+
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnFocusChangeListener;
+import android.view.View.OnTouchListener;
+import android.widget.EditText;
+
+public class ClearableEditText extends EditText implements OnTouchListener, OnFocusChangeListener {
+
+    public class ClearableEditTextWatcher implements TextWatcher {
+        ClearableEditText _parent;
+
+        ClearableEditTextWatcher(ClearableEditText parent) {
+            _parent = parent;
+        }
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            _parent.setClearIconVisibleIfThereIsText();
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+
+        }
+    }
+
+    public static enum Location {
+        LEFT(0), RIGHT(2);
+
+        final int idx;
+
+        private Location(int idx) {
+            this.idx = idx;
+        }
+    }
+
+    public interface Listener {
+        void didClearText();
+    }
+
+    public ClearableEditText(Context context) {
+        super(context);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public ClearableEditText(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    @Override
+    public void setOnTouchListener(OnTouchListener l) {
+        this.l = l;
+    }
+
+    @Override
+    public void setOnFocusChangeListener(OnFocusChangeListener f) {
+        this.f = f;
+    }
+
+    private Location loc = Location.RIGHT;
+
+    private Drawable xD;
+    private Listener listener;
+
+    private OnTouchListener l;
+    private OnFocusChangeListener f;
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        if (getDisplayedDrawable() != null) {
+            int x = (int) event.getX();
+            int y = (int) event.getY();
+
+            int left = getWidth() - getPaddingRight() - xD.getIntrinsicWidth();
+            int right = getWidth();
+
+            boolean tappedX = x >= left && x <= right && y >= 0 && y <= (getBottom() - getTop());
+            if (tappedX) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    setText("");
+                    if (listener != null) {
+                        listener.didClearText();
+                    }
+                }
+                return true;
+            }
+        }
+        if (l != null) {
+            return l.onTouch(v, event);
+        }
+        return false;
+    }
+
+    public void setClearIconVisibleIfThereIsText() {
+        String s = getText().toString();
+        setClearIconVisible(isNotEmpty(s));
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus) {
+        if (hasFocus) {
+            setClearIconVisibleIfThereIsText();
+        } else {
+            setClearIconVisible(false);
+        }
+        if (f != null) {
+            f.onFocusChange(v, hasFocus);
+        }
+    }
+
+    private boolean isNotEmpty(String text) {
+        return text.isEmpty() == false;
+    }
+
+    @Override
+    public void setCompoundDrawables(Drawable left, Drawable top, Drawable right, Drawable bottom) {
+        super.setCompoundDrawables(left, top, right, bottom);
+        initIcon();
+    }
+
+    private void init() {
+        super.setOnTouchListener(this);
+        super.setOnFocusChangeListener(this);
+        addTextChangedListener(new ClearableEditTextWatcher(this));
+        initIcon();
+        setClearIconVisible(false);
+    }
+
+    private void initIcon() {
+        xD = null;
+        if (loc != null) {
+            xD = getCompoundDrawables()[loc.idx];
+        }
+        if (xD == null) {
+            xD = getResources().getDrawable(android.R.drawable.presence_offline);
+        }
+        xD.setBounds(0, 0, xD.getIntrinsicWidth(), xD.getIntrinsicHeight());
+        int min = getPaddingTop() + xD.getIntrinsicHeight() + getPaddingBottom();
+        if (getSuggestedMinimumHeight() < min) {
+            setMinimumHeight(min);
+        }
+    }
+
+    private Drawable getDisplayedDrawable() {
+        return (loc != null) ? getCompoundDrawables()[loc.idx] : null;
+    }
+
+    protected void setClearIconVisible(boolean visible) {
+        Drawable[] cd = getCompoundDrawables();
+        Drawable displayed = getDisplayedDrawable();
+        boolean wasVisible = (displayed != null);
+        if (visible != wasVisible) {
+            Drawable x = visible ? xD : null;
+            super.setCompoundDrawables(cd[0], cd[1], x, cd[3]);
+        }
+    }
+}
